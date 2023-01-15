afb852ec1f31b18749a997b155287f4fa2c583aa
tom-sapletta-com <tom@sapletta.com>
2023-01-15 10:23:58 +0100

    update todo

---
+++ DOCS/echox.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+# USAGE
+# echox
+INPUT=$1
+
+# check if json
+
+
+# check if csv
+
+
+# else: txt
+echo "INPUT"
+
+# get One JSON = convert item to line ,
+# get One CSV = line /n
+# get One TXT = line /n
+# get all
+
+
